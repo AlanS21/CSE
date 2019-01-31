e5106e333a3f4fa669b4bc726ed70537aeb42cd3
AlanS21 <8co2@fresnou.org>
2019-01-31 14:37:23 -0800

    KEYERROR

--- Alan Sanchez Dictionary World Map.py
+++ Alan Sanchez Dictionary World Map.py
@@ -14,3 +14,22 @@ world_map = {
         }
     }
 }
+
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+current_node = world_map["R19A"]
+
+playing = True
+while playing:
+    print(current_node['NAME'])
+
+    command = input(">_")
+    if command in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command not recognized.")
