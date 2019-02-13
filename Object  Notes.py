746f8c90d683e5cef1c69b5dad98c7e21c727b73
AlanS21 <8co2@fresnou.org>
2019-02-13 14:34:06 -0800

    Alas

--- Object  Notes.py
+++ Object  Notes.py
@@ -1,5 +1,5 @@
 class Laptop(object):
-    def __init__(self, screen_resolution, extra_space, colour):
+    def __init__(self, screen_resolution, extra_space=1000, colour="Cobalt"):
         # Things that a laptop has.
         # Everything in this list should be relevant to the program
         self.processor = "Intel i5"
@@ -8,23 +8,49 @@ class Laptop(object):
         self.space_left = extra_space
         self.color = colour
         self.os = "Linux"
+        self.functioning = True
 
     def charge(self, time):
-        # Charge is already charged
-        if self.battery_left >= 100:
-            print("The computer is already charged.")
-            return
+        if self.functioning:
+            # Charge is already charged
+            if self.battery_left >= 100:
+                print("The computer is already charged.")
+                return
 
-        self.battery_left += time
-        # Computer is mostly charged
-        if self.battery_left > 100:
-            print("The computer completely charges.")
-
-        # Computer is not charged at all
+            self.battery_left += time  # This adds to the battery life
+            # Computer is mostly charged
+            if self.battery_left > 100:
+                print("The computer completely charges.")
+                self.battery_left = 100
+            # Computer is not charged at all
+            else:
+                print("The computer is now at %d%%" % self.battery_left)
         else:
-            print("The computer is now at %d%%" % self.battery_left)
+            print("It's broken good job.")
+
+    def smash(self):
+        self.functioning = False
+        print("I took the laptop....")
+        print()
+        print()
+        print()
+        print("...AND I THREW IT ON THE GROUND!!!!!")
+
+    def use(self, time):
+        self.battery_left -= time
+        print("You use the laptop for %s minutes" % time)
 
 
 my_computer = Laptop("1920x1080", 10000, "Black")
 your_computer = Laptop("10x10", 0, "Orange")
 wiebe_computer = Laptop("9000000000000000000x10000000000000000", 9999999999999999999999, "Awesome")
+default_computer = Laptop("1920x1080")
+
+my_computer.use(60)
+my_computer.charge(20)
+my_computer.charge(1000)
+my_computer.smash()
+my_computer.charge(20)
+
+your_computer.charge(20)
+your_computer.smash()
